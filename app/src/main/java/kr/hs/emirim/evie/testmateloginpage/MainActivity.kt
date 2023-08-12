0b2fa35d895fabeb28107cd2b00fa4e9624778fc
ezi-s-u <ljsgoo0617@gmail.com>
2023-08-12 16:02:27 +0900

    init project

---
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/MainActivity.kt
@@ -0,0 +1,26 @@
+package kr.hs.emirim.evie.testmateloginpage
+
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var imageButton: ImageButton
+    private var isImageVisible = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.alarm_page)
+
+        imageButton = findViewById(R.id.password_btn)
+        imageButton.setOnClickListener {
+            isImageVisible = !isImageVisible
+            if (isImageVisible) {
+                imageButton.setImageResource(R.drawable.visible_icon)
+            } else {
+                imageButton.setImageResource(R.drawable.invisible_icon)
+            }
+        }
+    }
+}
