4ff8ce524490b4b02dfac7fb05e24c7109d68185
ezi-s-u <ljsgoo0617@gmail.com>
2023-08-26 13:22:36 +0900

    [Feat, Refactor] : Goal(학습목표) 페이지 유동적으로 수정중, 레이아웃 수정

--- app/src/main/java/kr/hs/emirim/evie/testmateloginpage/GoalActivity.kt
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/GoalActivity.kt
@@ -2,9 +2,19 @@ package kr.hs.emirim.evie.testmateloginpage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class GoalActivity : AppCompatActivity() {
+
+    private lateinit var goalRecycleView : RecyclerView
+    lateinit var adapter : GoalAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide() // Android 앱의 액션바(작업 표시줄)를 숨기는 코드
+
+        goalRecycleView = findViewById(R.id.goalRecyclerView)
+        goalRecycleView.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/kr/hs/emirim/evie/testmateloginpage/Model/GoalModel.kt
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/Model/GoalModel.kt
@@ -1,4 +1,9 @@
 package kr.hs.emirim.evie.testmateloginpage.Model
 
 class GoalModel {
+    private var id : Int = 0
+    private var status : Int = 0
+    private var txt : String? = null
+
+    
 }
