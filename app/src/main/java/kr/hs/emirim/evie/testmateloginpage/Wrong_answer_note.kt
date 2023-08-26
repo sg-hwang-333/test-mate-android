afb601d7fab8723180c7b1569f3c302ab9c81559
LO-VE-0 <hsg151515@naver.com>
2023-08-26 17:58:50 +0900

    [Feat] : 메인 페이지, 오답 노트 페이지, 캘린더 페이지

---
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/Wrong_answer_note.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.DialogFragment
+
+class Wrong_answer_note : AppCompatActivity() {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_wrong_answer_note)
+
+        }
+    }
