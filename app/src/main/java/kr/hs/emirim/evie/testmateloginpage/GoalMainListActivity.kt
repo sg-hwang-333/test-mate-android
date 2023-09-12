775302318f5cb7a4b7201b5c5dd01d26ef3db618
ezi-s-u <ljsgoo0617@gmail.com>
2023-09-12 23:49:22 +0900

    [Rename] : GoalMainActivity -> GoalMainListActivity 이름 수정

--- app/src/main/java/kr/hs/emirim/evie/testmateloginpage/GoalMainListActivity.kt
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/GoalMainListActivity.kt
@@ -3,7 +3,7 @@ package kr.hs.emirim.evie.testmateloginpage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class GoalMainActivity : AppCompatActivity() {
+class GoalMainListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.goal_main_page)
