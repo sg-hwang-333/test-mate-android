7af6cfe83868a508a9af46f8243f82e3ec7692ad
ezi-s-u <ljsgoo0617@gmail.com>
2023-09-15 04:08:38 +0900

    [Feat] : HomeActivity에서 과목 추가 기능 구현중

--- app/src/main/java/kr/hs/emirim/evie/testmateloginpage/subject/AddSubjectActivity.kt
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/subject/AddSubjectActivity.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import kr.hs.emirim.evie.testmateloginpage.R
@@ -12,7 +13,7 @@ import kr.hs.emirim.evie.testmateloginpage.R
 const val SUBJECT_NAME = "새로운 과목"
 
 class AddSubjectActivity : AppCompatActivity() {
-    private lateinit var addSubjectName: TextInputEditText
+    private lateinit var addSubjectName: EditText
     lateinit var finishBtn : androidx.appcompat.widget.AppCompatImageButton
 
     @SuppressLint("MissingInflatedId")
@@ -21,13 +22,15 @@ class AddSubjectActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit_subjects) //  TODO : 과목 추가 화면
         supportActionBar?.hide()
 
+        addSubjectName = findViewById(R.id.subjectName)
+
         finishBtn = findViewById(R.id.finish_edit_subject)
         finishBtn.setOnClickListener{
             finish()
         }
 
         findViewById<Button>(R.id.subjectConfirmBtn).setOnClickListener {
-            finish()
+            addSubject()
         }
 //        addSubjectName = findViewById(R.id.subjectName)
     }
@@ -35,14 +38,6 @@ class AddSubjectActivity : AppCompatActivity() {
     private fun addSubject() {
         val resultIntent = Intent()
 
-//        if (addSubjectName.text.isNullOrEmpty()) {
-//            setResult(Activity.RESULT_CANCELED, resultIntent)
-//        } else {
-//            val name = addSubjectName.text.toString()
-//            resultIntent.putExtra(SUBJECT_NAME, name)
-//            setResult(Activity.RESULT_OK, resultIntent)
-//        }
-//        finish()
         if (addSubjectName.text.isNullOrEmpty()) {
             setResult(Activity.RESULT_CANCELED, resultIntent)
         } else {
@@ -51,5 +46,18 @@ class AddSubjectActivity : AppCompatActivity() {
             setResult(Activity.RESULT_OK, resultIntent)
         }
         finish()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intentData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intentData)
+
+        /* Inserts flower into viewModel. */
+//        if (resultCode == Activity.RESULT_OK) {
+//            intentData?.let { data ->
+//                val subjectName = data.getStringExtra(SUBJECT_NAME)
+//
+//                SubjectsListVIewModel.insertSubject(subjectName) ///////////////////////////////////////// insertFlower
+//            }
+//        }
     }
 }
