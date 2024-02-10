ab6c5151340e3a8d575ea660783dd7d929159fd1
Seulgi Hwang <hsg151515@naver.com>
2024-02-10 18:48:44 +0900

    [FEAT] 회원가입 창 이동 구현

---
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/signup/terms_privacy_collection.kt
@@ -0,0 +1,25 @@
+package kr.hs.emirim.evie.testmateloginpage.signup
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kr.hs.emirim.evie.testmateloginpage.R
+
+class terms_privacy_collection : AppCompatActivity() {
+    lateinit var backBtn : ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terms_privacy_collection)
+        supportActionBar?.hide()
+
+        backBtn = findViewById(R.id.backBtn)
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this, signup_privacy::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent)
+        }
+
+    }
+}
