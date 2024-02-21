fb4330aef88d6cfd3eb8f65a9d769765e26f52ec
ezi-s-u <ljsgoo0617@gmail.com>
2024-02-21 23:44:54 +0900

    [Feat] : 그래프 적용

--- app/src/main/java/kr/hs/emirim/evie/testmateloginpage/home/HomeActivity.kt
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/home/HomeActivity.kt
@@ -40,6 +40,17 @@ import kr.hs.emirim.evie.testmateloginpage.subject.SubjectsListViewModelFactory
 import kr.hs.emirim.evie.testmateloginpage.subject.data.Subject
 
 class HomeActivity : AppCompatActivity() {
+    // 시험기록 데이터 생성
+    val testRecordDataList: List<TestData> = listOf(
+        TestData("1학년 2학기 기말",75),
+        TestData("1학년 2학기 중간",60),
+        TestData("1학년 1학기 기말",80),
+        TestData("1학년 1학기 중간",100),
+        TestData("@학년 @학기 중간",89),
+        TestData("@학년 @학기 중간",91),
+        TestData("@학년 @학기 중간",78),
+        TestData("@학년 @학기 중간",96)
+    )
 
     lateinit var navHome: ImageButton
     lateinit var navGoal: ImageButton
@@ -69,7 +80,68 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // 성적 그래프
+        val linechart = findViewById<LineChart>(R.id.test_record_chart)
+        val xAxis = linechart.xAxis
 
+        val entries: MutableList<Entry> = mutableListOf() // Entry : 데이터 포인트를 나타내는 클래스
+        for (i in testRecordDataList.indices){
+            entries.add(Entry(i.toFloat(), testRecordDataList[i].score.toFloat()))
+        }
+        val lineDataSet = LineDataSet(entries,"entries")
+
+        lineDataSet.apply {
+            color = resources.getColor(R.color.green_500, null) // 선 색깔
+            circleRadius = 5f
+            lineWidth = 3f
+            setCircleColor(resources.getColor(R.color.green_500, null))
+            circleHoleColor = resources.getColor(R.color.green_500, null)
+            setDrawHighlightIndicators(false)
+            setDrawValues(true) // 숫자표시
+            valueTextColor = resources.getColor(R.color.black, null)
+            valueFormatter = DefaultValueFormatter(0)  // 소숫점 자릿수 설정
+            valueTextSize = 10f
+        }
+
+        //차트 전체 설정
+        linechart.apply {
+            axisRight.isEnabled = true   //y축 사용여부
+            axisLeft.isEnabled = true
+            legend.isEnabled = false    //legend 사용여부
+            description.isEnabled = false //주석
+            isDragXEnabled = true   // x 축 드래그 여부
+            isScaleYEnabled = false //y축 줌 사용여부
+            isScaleXEnabled = false //x축 줌 사용여부
+        }
+
+        //X축 설정
+        xAxis.apply {
+            setDrawGridLines(false)
+            setDrawAxisLine(true)
+            setDrawLabels(true)
+            position = XAxis.XAxisPosition.BOTTOM
+            valueFormatter =
+                EditTestRecordActivity.XAxisCustomFormatter(changeTestDateText(testRecordDataList))
+            textColor = resources.getColor(R.color.black, null)
+            textSize = 10f
+            labelRotationAngle = 0f
+            setLabelCount(10, true)
+        }
+
+        val horizontalScrollView = findViewById<HorizontalScrollView>(R.id.scroll_view_graph)
+        horizontalScrollView.post{
+            horizontalScrollView.scrollTo(
+                linechart.width,
+                0
+            )
+        }
+
+        linechart.apply {
+            data = LineData(lineDataSet)
+            notifyDataSetChanged() //데이터 갱신
+            invalidate() // view갱신
+        }
+        // 성적 그래프
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         val view = binding.root
@@ -164,6 +236,22 @@ class HomeActivity : AppCompatActivity() {
         }
     } // onCreate
 
+    // 성적 그래프
+    fun changeTestDateText(dataList: List<TestData>): List<String> {
+        val dataTextList = ArrayList<String>()
+        for (i in dataList.indices) {
+            dataTextList.add(dataList[i].testDate)
+        }
+        return dataTextList
+    }
+
+    class XAxisCustomFormatter(val xAxisData: List<String>) : ValueFormatter() {
+        override fun getFormattedValue(value: Float): String {
+            return xAxisData[(value).toInt()]
+        }
+
+    }
+    // 성적 그래프
 
 
     private fun adapterOnClick(subject: Subject) {
