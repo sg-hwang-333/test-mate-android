f579bd0316f85a633e7c3e01315495ac26f4a1a2
ezi-s-u <ljsgoo0617@gmail.com>
2024-06-11 15:12:42 +0900

    [ing] : 오답노트 과목 버튼 수정중 (작동안함)

--- app/src/main/java/kr/hs/emirim/evie/testmateloginpage/subject/WrongAnswerSubjectAdapter.kt
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/subject/WrongAnswerSubjectAdapter.kt
@@ -1,10 +1,12 @@
 package kr.hs.emirim.evie.testmateloginpage.subject
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -12,30 +14,43 @@ import kr.hs.emirim.evie.testmateloginpage.R
 import kr.hs.emirim.evie.testmateloginpage.subject.data.Subject
 
 
-class WrongAnswerSubjectAdapter(private val onClick: (Subject) -> Unit) :
+class WrongAnswerSubjectAdapter(private val onClick: (Subject, Int) -> Unit) :
     ListAdapter<Subject, WrongAnswerSubjectAdapter.WrongAnswerSubjectHolder>(WrongAnswerSubjectDiffCallback) {
 
-    inner class WrongAnswerSubjectHolder(itemView: View, val onClick: (Subject) -> Unit) :
+    private var selectedPosition: Int = RecyclerView.NO_POSITION
+
+    inner class WrongAnswerSubjectHolder(itemView: View, val onClick: (Subject, Int) -> Unit) :
         RecyclerView.ViewHolder(itemView) {
         val subjectTextView: TextView = itemView.findViewById(R.id.wrongAnswerSubject)
         var currentSubject: Subject? = null
 
-        private lateinit var subjectEditBtn: Button
+//        private lateinit var subjectEditBtn: Button
 
         init {
             itemView.setOnClickListener {
                 currentSubject?.let {
-                    onClick(it)
+                    onClick(it, absoluteAdapterPosition)
+                    updateSelectedPosition(absoluteAdapterPosition)
                 }
             }
         }
 
 
         /* UI에 정보 바인딩(넣는 메서드) */
-        fun bind(subject: Subject) {
+        fun bind(subject: Subject, isSelected: Boolean) {
             currentSubject = subject
 
             subjectTextView.setText(subject.subjectName)
+
+//            Log.d("WANsubjectBtn", isSelected)
+
+            if (isSelected) {
+                itemView.setBackgroundResource(R.drawable.btn_border_bottom_green)
+                subjectTextView.setTextColor(ContextCompat.getColor(itemView.context, R.color.green_500))
+            } else {
+                itemView.setBackgroundResource(0)
+                subjectTextView.setTextColor(ContextCompat.getColor(itemView.context, R.color.black_300))
+            }
         }
 
     }
@@ -49,7 +64,14 @@ class WrongAnswerSubjectAdapter(private val onClick: (Subject) -> Unit) :
 
     override fun onBindViewHolder(holder: WrongAnswerSubjectHolder, position: Int) {
         val subject = getItem(position)
-        holder.bind(subject)
+        holder.bind(subject, position == selectedPosition)
+    }
+
+    fun updateSelectedPosition(position: Int) {
+        val previousPosition = selectedPosition
+        selectedPosition = position
+        notifyItemChanged(previousPosition)
+        notifyItemChanged(selectedPosition)
     }
 
 }
