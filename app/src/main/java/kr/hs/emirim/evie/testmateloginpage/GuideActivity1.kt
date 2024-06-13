c71ce149b7b7bc633bfe7ea3131703ae17f3623d
sg-hwang-333 <hsg151515@naver.com>
2024-06-13 18:52:18 +0900

    [FEAT] 앱 실행시 Guide 파일 넘어가게 하기

--- app/src/main/java/kr/hs/emirim/evie/testmateloginpage/GuideActivity1.kt
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/GuideActivity1.kt
@@ -1,11 +1,22 @@
 package kr.hs.emirim.evie.testmateloginpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class GuideActivity1 : AppCompatActivity() {
+
+    lateinit var nextBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.signup_page)
+        setContentView(R.layout.guide_page1)
+        nextBtn = findViewById(R.id.next1)
+
+        nextBtn.setOnClickListener {
+            val intent = Intent(this, GuideActivity2::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/kr/hs/emirim/evie/testmateloginpage/GuideActivity3.kt
+++ app/src/main/java/kr/hs/emirim/evie/testmateloginpage/GuideActivity3.kt
@@ -1,11 +1,23 @@
 package kr.hs.emirim.evie.testmateloginpage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kr.hs.emirim.evie.testmateloginpage.login.LoginActivity
 
 class GuideActivity3 : AppCompatActivity() {
+    lateinit var nextBtn : Button;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.guide_page3)
+
+        nextBtn = findViewById(R.id.next1)
+
+        nextBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            startActivity(intent)
+        }
     }
 }
